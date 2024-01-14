3b69529080f01cdb515294144806093f48e60dc9
leeje0506 <137889331+leeje0506@users.noreply.github.com>
2024-01-14 18:55:08 +0900

    Title: 테트리스 블럭 안의 합 최대화 하기, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240114/테트리스 블럭 안의 합 최대화 하기/max-sum-of-tetris-block.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    // 각 블록의 모든 가능한 모양
+    static int[][][] block = {
+        {{0, 0}, {0, 1}, {0, 2}, {0, 3}}, // ㅡ
+        {{0, 0}, {1, 0}, {2, 0}, {3, 0}}, // ㅣ
+        {{0, 0}, {0, 1}, {1, 0}, {1, 1}}, // ㅁ
+        {{0, 0}, {1, 0}, {2, 0}, {2, 1}}, // ㄴ
+        {{0, 0}, {0, 1}, {0, 2}, {1, 0}}, // ㅗ
+        {{0, 0}, {1, 0}, {1, 1}, {2, 1}}, // z
+        {{0, 1}, {1, 1}, {1, 0}, {2, 0}}, // s
+        {{0, 0}, {0, 1}, {0, 2}, {1, 2}}, // ㄱ
+        {{0, 0}, {1, 0}, {1, 1}, {1, 2}}  // ㅜ
+    };
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] map = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                for (int k = 0; k < block.length; k++) {
+                    int sum = 0;
+                    for (int l = 0; l < 4; l++) {
+                        int nx = i + block[k][l][0];
+                        int ny = j + block[k][l][1];
+                        if (0 <= nx && nx < n && 0 <= ny && ny < m) {
+                            sum += map[nx][ny];
+                        } else {
+                            sum = 0;
+                            break;
+                        }
+                    }
+                    max = Math.max(max, sum);
+                }
+            }
+        }
+        System.out.println(max);
+    }
+}
